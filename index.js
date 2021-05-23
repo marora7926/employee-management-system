89151e62fce6dc414a7155536432a08d14fb5297
Mohit Arora <mohit.arora@sydney.edu.au>
2021-05-23 17:08:48 +1000

    added inquirer prompts

--- index.js
+++ index.js
@@ -14,5 +14,60 @@ connection.connect((err) => {
     if (err) throw err;
     runTasks();
   });
-  
+
+const runTasks = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'tasks',
+        message: 'What would you like to do?',
+        choices: [
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'View departments',
+        'View roles',
+        'View employees',
+        'Update employee role',
+        'exit',
+        ],
+    })
+    .then((answer) => {
+        switch (answer.task) {
+            case 'Add a department':
+                addDepartment();
+                runTasks();
+            break;
+            case 'Add a role':
+                addRole();
+                runTasks();
+            break;
+            case 'Add an employee':
+                addEmployee();
+                runTasks();
+            break;
+            case 'View departments':
+                viewDepartments();
+                runTasks();
+            break;
+            case 'View roles':
+                viewRoles();
+                runTasks();
+            break;
+            case 'View employees':
+                viewEmployees();
+                runTasks();
+            break;
+            case 'Update employee role':
+                updateEmployee();
+                runTasks();
+            break;
+            case 'Exit':
+                connection.end();
+            break;
+            default:
+                console.log(`Invalid action: ${answer.task}`);
+            break;
+        }
+    });
+};
 
